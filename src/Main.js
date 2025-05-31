48e648170d824b7d22420f98cb303c1cb24caf42
ZeeshanTariq1 <zeetariq765@gmail.com>
2025-05-31 14:50:24 +0500

    addingnew details like header nav main and footer

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+        <h1>Welcome to Little Lemon Restaurant</h1>
+        <p>Your favourite Mediterranian Cuisine</p>
+    </main>
+  )
+}
+
+export default Main
