fb63a5e5e47ec480ad735025137cc8fd8f67066d
ZeeshanTariq1 <zeetariq765@gmail.com>
2025-05-31 15:23:12 +0500

    adding styles

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-export const Nav = () => {
-  return (
-    <nav>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#menu">Menu</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-    </nav>
-  )
-}
+const Nav = () => (
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/menu">Menu</a></li>
+      <li><a href="/reservations">Reservations</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/order">Order Online</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+  </nav>
+);
 
-export default Nav
+export default Nav;
